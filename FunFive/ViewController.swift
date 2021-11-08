168b8669149f1c0b81a9e133b40b11b959b960f3
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-11-08 14:02:10 -0600

    Initial Commit

---
+++ FunFive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunFive
+//
+//  Created by Amor on 2021/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
